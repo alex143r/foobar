f3bb7c112edd239bef88aadbb59cdab4f4ecc0f0
Jonas <jona71e2@stud.kea.dk>
2020-12-02 01:25:23 +0100

    imports in App.js and App.scss

--- src/App.js
+++ src/App.js
@@ -2,29 +2,26 @@ import React, { useState, useEffect } from "react";
 
 import { get } from "./modules/rest";
 
+import Storage from "./components/Storage";
+
 import "./App.scss";
 
 function App() {
-	const [facts, setFacts] = useState([]);
-	useEffect(() => {
-		get(setFacts);
-		const interval = setInterval(() => {
-			get(setFacts);
-		}, 2000);
-		return () => clearInterval(interval);
-	}, []);
-	console.log(facts);
-	return (
-		<div className="App">
-			{facts.bar !== undefined ? (
-				<div>
-					<h1>{facts.bar.name}</h1>
-					<p>{facts.bartenders[0].name}</p>
-					<p>{facts.bartenders[0].statusDetail}</p>
-				</div>
-			) : null}
-		</div>
-	);
+  const [facts, setFacts] = useState([]);
+  useEffect(() => {
+    get(setFacts);
+    const interval = setInterval(() => {
+      get(setFacts);
+    }, 2000);
+    return () => clearInterval(interval);
+  }, []);
+
+  //console.log(facts);
+  return (
+    <div className="App">
+      <Storage storage={facts.storage} taps={facts.taps} />
+    </div>
+  );
 }
 
 export default App;
