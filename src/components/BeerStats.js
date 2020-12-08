0fa9ba1f3cfd3fe5ef02e465418fc9b6f619af20
Alexander <alex143r@stud.kea.dk>
2020-12-08 10:58:38 +0100

    beer stats start

--- src/components/BeerStats.js
+++ src/components/BeerStats.js
@@ -1,7 +1,27 @@
 import React, { useState } from "react";
-import beers from "./BeerSold";
 
-export default function BeerStats() {
-  console.log(beers);
-  return <h1>yo</h1>;
+export default function BeerStats({ serving }) {
+  const [beers, setBeers] = useState([...serving]);
+  const [beerNames, setBeersNames] = useState([]);
+
+  /*
+  function initialCount() {
+    let initCount = 0;
+    beers.forEach((order) => {
+      initCount += order.order.length;
+    });
+    return initCount;
+  }
+*/
+  //const [count, setCount] = useState(initialCount);
+
+  serving.forEach((order) => {
+    const findItem = beers.find((item) => item.id === order.id);
+
+    if (findItem === undefined) {
+      setBeers([...beers, order]);
+      /* setCount((prevCount) => prevCount + order.order.length);*/
+    }
+  });
+  return <h1>{beers[1].name} </h1>;
 }
