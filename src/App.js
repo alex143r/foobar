e84424e3843cf0a349b5fb8ea6be2b318edba972
Jonas <jona71e2@stud.kea.dk>
2020-12-09 00:55:02 +0100

    tick hook created and passed down to QueueHistory

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import React, { useState, useEffect } from "react";
 import { get } from "./modules/rest";
 
 import Storage from "./components/Storage";
+import QueueHistoryData from "./components/QueueHistoryData";
 
 import "./App.scss";
+import QueueHistory from "./components/QueueHistory";
 
 function App() {
   const [facts, setFacts] = useState([]);
+  const [tick, setTick] = useState(0);
   useEffect(() => {
     get(setFacts);
     const interval = setInterval(() => {
       get(setFacts);
+      setTick((tick) => tick + 1);
     }, 2000);
     return () => clearInterval(interval);
   }, []);
@@ -20,6 +24,12 @@ function App() {
   return (
     <div className="App">
       <Storage storage={facts.storage} taps={facts.taps} />
+      <QueueHistoryData
+        queue={facts.queue}
+        time={facts.timestamp}
+        serving={facts.serving}
+        tick={tick}
+      />
     </div>
   );
 }
--- src/components/ListContainer.js
+++ src/components/ListContainer.js
@@ -5,7 +5,8 @@ import StorageAmount from "./StorageAmount";
 export default function ListContainer({ storage, taps }) {
   //console.log(storage, taps);
   const [filter, setFilter] = useState("all");
-  const [sortKey, setSortKey] = useState("name");
+  const [sortKey, setSortKey] = useState("length");
+  const [sortDirection, setSortDirection] = useState("asc");
 
   const FullDataObject = {
     name: "",
@@ -41,6 +42,17 @@ export default function ListContainer({ storage, taps }) {
 
   let filteredList = fullDataArray;
 
+  // console.log(
+  //   fullDataArray.sort((a, b) => {
+  //     console.log(
+  //       a.taps.forEach((tap) => console.log("A", tap.level)),
+  //       b.taps.forEach((tap) => console.log("B", tap.level))
+  //     );
+  //   })
+  // );
+
+  //console.log("result", result);
+
   if (filter !== "all") {
     filteredList = fullDataArray.filter(
       (beer) => beer.taps.length > 0 === filter
@@ -50,22 +62,82 @@ export default function ListContainer({ storage, taps }) {
   //console.log("LIST", filteredList);
 
   filteredList = filteredList.sort((a, b) => {
-    if (a[sortKey] > b[sortKey]) {
-      return 1;
+    if (sortKey === "length") {
+      if (a.taps[sortKey] > b.taps[sortKey]) {
+        return -1;
+      } else {
+        return 1;
+      }
+    } else if (sortKey === "inUse") {
+      console.log("inUSE SORT", a, b.taps[0]);
+      if (b.taps[sortKey] - a.taps[sortKey]) {
+        return -1;
+      } else {
+        return 1;
+      }
     } else {
-      return -1;
+      if (a[sortKey] > b[sortKey]) {
+        return sortDirection === "asc" ? 1 : -1;
+      } else {
+        return sortDirection === "asc" ? -1 : 1;
+      }
     }
   });
 
   return (
     <>
-      <button onClick={() => setFilter("all")}>Filter: All</button>
-      <button onClick={() => setFilter(true)}>Filter: On tap</button>
-      <button onClick={() => setFilter(false)}>Filter: Not on tap</button>
+      {/* <button onClick={() => setFilter("all")}>Filter: All</button> */}
+      <button
+        className={filter === true ? "button-active" : "button-default"}
+        onClick={() => setFilter(filter === true ? "all" : true)}
+      >
+        On tap
+      </button>
+      <button
+        className={filter === false ? "button-active" : "button-default"}
+        onClick={() => setFilter(filter === false ? "all" : false)}
+      >
+        Not on tap
+      </button>
 
-      <button onClick={() => setSortKey("name")}>Sort: Name</button>
-      <button onClick={() => setSortKey("amount")}>Sort: Amount</button>
-      <button onClick={() => setSortKey("taps")}>Sort: Taps</button>
+      <button
+        onClick={() => {
+          setSortKey("name");
+          setSortDirection(sortDirection === "asc" ? "desc" : "asc");
+        }}
+      >
+        Sort: Navn
+      </button>
+      <button
+        onClick={() => {
+          setSortKey("amount");
+          setSortDirection(sortDirection === "asc" ? "desc" : "asc");
+        }}
+      >
+        Sort: Lager
+      </button>
+      <button onClick={() => setSortKey("length")}>
+        Sort: Antal aktive taps
+      </button>
+      <button onClick={() => setSortKey("inUse")}>Sort: inUse</button>
+
+      <select
+        sorting={filteredList}
+        onChange={(evt) => {
+          console.log("select change", evt.target.value);
+          setSortKey(evt.target.value);
+          setSortDirection(sortDirection === "asc" ? "desc" : "asc");
+        }}
+        onClick={(evt) => {
+          console.log("CLICK Event", evt.target.value);
+        }}
+      >
+        <option>Sort:</option>
+        <option value="name">Navn</option>
+        <option value="amount">Lager</option>
+        <option value="length">Antal aktive taps</option>
+      </select>
+
       <ul>
         {filteredList.map((beer) => {
           return (
