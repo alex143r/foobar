fb9de57256a3d820f26cc693263c1014e07a039a
Jonas <jona71e2@stud.kea.dk>
2020-12-15 05:27:14 +0100

    ListContainer.js cleaning plus comments

--- src/components/ListContainer.js
+++ src/components/ListContainer.js
@@ -4,76 +4,62 @@ import StorageAmount from "./StorageAmount";
 
 export default function ListContainer({ storage, taps }) {
   //console.log(storage, taps);
+  // HOOKS
   const [filter, setFilter] = useState("all");
   const [sortKey, setSortKey] = useState("length");
-  const [sortDirection, setSortDirection] = useState("asc");
+  const [sortDirection, setSortDirection] = useState("desc");
 
+  //Object prototype
   const FullDataObject = {
     name: "",
     amount: null,
     taps: [],
   };
 
+  // Array til data
   let fullDataArray = [];
 
+  //Tilpasning af data til object prototype.
   function prepareObjects(storage) {
     fullDataArray = storage.map(prepareObject);
   }
 
   function prepareObject(data) {
-    //console.log(data);
     const fullData = Object.create(FullDataObject);
 
     fullData.name = data.name;
     fullData.amount = data.amount;
 
+    //Looper over taps. For hver tap findes den pågældende øl fra storage og indsættes
     taps.map((tap) => {
-      //console.log(tap);
       if (tap.beer === fullData.name) {
         fullData.taps = [...fullData.taps, { ...tap }];
       }
+      return tap;
     });
 
     return fullData;
   }
 
   prepareObjects(storage);
-  //console.log("DATA_____", fullDataArray);
 
   let filteredList = fullDataArray;
 
-  // console.log(
-  //   fullDataArray.sort((a, b) => {
-  //     console.log(
-  //       a.taps.forEach((tap) => console.log("A", tap.level)),
-  //       b.taps.forEach((tap) => console.log("B", tap.level))
-  //     );
-  //   })
-  // );
-
-  //console.log("result", result);
-
+  //Tilpasser filteredList til staten af filter
   if (filter !== "all") {
-    filteredList = fullDataArray.filter(
-      (beer) => beer.taps.length > 0 === filter
+    filteredList = filteredList.filter((beer) =>
+      filter ? beer.taps.length > 0 : beer.taps.length === 0
     );
   }
-  //const filteredList = fullDataArray.filter((beer) => beer.name.length > 10);
-  //console.log("LIST", filteredList);
 
+  //Tilpasser filteredList til staten af sortKey
   filteredList = filteredList.sort((a, b) => {
+    //Tjekker først om der sorteres efter array-længden på taps.
     if (sortKey === "length") {
       if (a.taps[sortKey] > b.taps[sortKey]) {
-        return -1;
+        return sortDirection === "asc" ? 1 : -1;
       } else {
-        return 1;
-      }
-    } else if (sortKey === "inUse") {
-      console.log("inUSE SORT", a, b.taps[0]);
-      if (b.taps[sortKey] - a.taps[sortKey]) {
-        return -1;
-      } else {
-        return 1;
+        return sortDirection === "asc" ? -1 : 1;
       }
     } else {
       if (a[sortKey] > b[sortKey]) {
@@ -86,73 +72,52 @@ export default function ListContainer({ storage, taps }) {
 
   return (
     <>
-      {/* <button onClick={() => setFilter("all")}>Filter: All</button> */}
-      <button
-        className={filter === true ? "button-active" : "button-default"}
-        onClick={() => setFilter(filter === true ? "all" : true)}
-      >
-        On tap
-      </button>
-      <button
-        className={filter === false ? "button-active" : "button-default"}
-        onClick={() => setFilter(filter === false ? "all" : false)}
-      >
-        Not on tap
-      </button>
-
-      <button
-        onClick={() => {
-          setSortKey("name");
-          setSortDirection(sortDirection === "asc" ? "desc" : "asc");
-        }}
-      >
-        Sort: Navn
-      </button>
-      <button
-        onClick={() => {
-          setSortKey("amount");
-          setSortDirection(sortDirection === "asc" ? "desc" : "asc");
-        }}
-      >
-        Sort: Lager
-      </button>
-      <button onClick={() => setSortKey("length")}>
-        Sort: Antal aktive taps
-      </button>
-      <button onClick={() => setSortKey("inUse")}>Sort: inUse</button>
-
-      <select
-        sorting={filteredList}
-        onChange={(evt) => {
-          console.log("select change", evt.target.value);
-          setSortKey(evt.target.value);
-          setSortDirection(sortDirection === "asc" ? "desc" : "asc");
-        }}
-        onClick={(evt) => {
-          console.log("CLICK Event", evt.target.value);
-        }}
-      >
-        <option>Sort:</option>
-        <option value="name">Navn</option>
-        <option value="amount">Lager</option>
-        <option value="length">Antal aktive taps</option>
-      </select>
-
+      <nav>
+        <button
+          className={filter === true ? "button-active" : "button-default"}
+          onClick={() => setFilter(filter === true ? "all" : true)}
+        >
+          Aktive
+        </button>
+        <button
+          className={filter === false ? "button-active" : "button-default"}
+          onClick={() => setFilter(filter === false ? "all" : false)}
+        >
+          Passive
+        </button>
+        <select
+          sorting={filteredList}
+          onChange={(evt) => {
+            //Variabel, der bruges til at sætte sortKey og sortDirection
+            const userInput = evt.target.value.split(" ");
+            //outputtet efter .split(" "):
+            // et array hvor index 0 = sortKey og index 1 = sortDirection
+            setSortKey(userInput[0]);
+            setSortDirection(userInput[1]);
+          }}
+        >
+          <option>Sort:</option>
+          <option value="name asc">Navn (a-z)</option>
+          <option value="name desc">Navn (z-a)</option>
+          <option value="amount asc">Lager (lav-høj)</option>
+          <option value="amount desc">Lager (høj-lav)</option>
+          <option value="length asc">Aktive taps (lav-høj)</option>
+          <option value="length desc">Aktive taps (høj-lav)</option>
+        </select>
+      </nav>
       <ul>
         {filteredList.map((beer) => {
           return (
             <li key={beer.name}>
-              <h2>{beer.name}</h2>
+              <h2 className="list-item-header">{beer.name}</h2>
               <div className="progress-container">
                 {beer.taps.length > 0 ? (
                   <TapLevel taps={beer.taps} />
                 ) : (
-                  <h3>Not on tap</h3>
+                  <h3>Ikke på tap</h3>
                 )}
               </div>
-              {/* <h3>Lager</h3>
-            <h3>{beer.amount}</h3> */}
-              <StorageAmount amount={beer.amount} />
+              <StorageAmount className="storage-amount" amount={beer.amount} />
             </li>
           );
         })}
