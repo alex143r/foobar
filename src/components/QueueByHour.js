f16aa491ad8ad4b634531638a270e149899ad345
Jonas <jona71e2@stud.kea.dk>
2020-12-15 15:28:35 +0100

    missing user interaction in QueueByhour.js

--- src/components/QueueByHour.js
+++ src/components/QueueByHour.js
@@ -1,7 +1,130 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Bar, Line } from "react-chartjs-2";
+
+export default function QueueByHour() {
+  const [queueAverage, setQueueAverage] = useState([]);
+  const [hour, setHour] = useState([]);
+  const [fullData, setFullData] = useState([]);
+
+  //const [interval, setInterval] = [];
+
+  const url =
+    "https://foobar-edfd.restdb.io/rest/foobar-queue?h={%22$groupby%22:[%22$HOUR:date%22],%20%22$aggregate%22:%20[%22AVG:length%22]}";
+  const key = "5f9c2a96231ba42851b49f89";
+
+  useEffect(() => {
+    function fetchData() {
+      fetch(url, {
+        method: "get",
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+          "x-apikey": key,
+          "cache-control": "no-cache",
+        },
+      })
+        .then((e) => e.json())
+        .then((data) => {
+          //setQueueAverage([e]);
+          handleData(data);
+          //console.log("DATA???", data);
+        });
+    }
+    console.log("HELLO");
+    fetchData();
+  }, []);
+
+  function handleData(data) {
+    console.log("handleData", data);
+
+    for (const property in data) {
+      //console.log(data, property);
+      //console.log(`${property}: ${data[property]["AVG length"]}`);
+      setQueueAverage((queueAverage) =>
+        queueAverage.concat(data[property]["AVG length"].toFixed(2))
+      );
+      setHour((hour) => hour.concat(property));
+    }
+  }
+
+  console.log(queueAverage, "STATE", hour);
+
+  //const filteredByHour = hour.filter((hour) => hour >= 12 && hour <= 22);
+  //console.log(filteredByHour);
+
+  //const findIndexStart = hour.findIndex((hour) => hour == 12);
+  //const findIndexEnd = hour.findIndex((hour) => hour == 22);
+  //console.log(findIndexStart, findIndexEnd, "index");
+
+  const data = {
+    labels: hour,
+    datasets: [
+      {
+        label: "Kø-gennemsnit pr. time",
+        data: queueAverage,
+        backgroundColor: ["rgba(99, 139, 174, 0.2)"],
+        borderColor: ["#638bae"],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options = {
+    title: {
+      display: true,
+      text: `INDSÆT TEXT HER`,
+      lineHeight: "2",
+    },
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            autoSkip: true,
+            maxTicksLimit: 10,
+            beginAtZero: true,
+            min: 0,
+            suggestedMax: 7,
+          },
+        },
+      ],
+    },
+  };
+
+  return (
+    <>
+      {/* <button
+        onClick={() => {
+          setHour((hour) => {
+            hour.slice(hour.indexOf("17"), hour.indexOf("21"));
+            console.log(hour);
+          });
+          setQueueAverage((queueAverage) => {
+            queueAverage.slice(
+              queueAverage.indexOf("17"),
+              queueAverage.indexOf("21")
+            );
+          });
+        }}
+      >
+        Interval
+      </button> */}
+      <Line data={data} options={options} />
+      <Bar data={data} options={options} />
+    </>
+  );
+}
 
 // fetch data fra restdb: https://foobar-edfd.restdb.io/rest/foobar-queue?h={%22$groupby%22:[%22$HOUR:date%22],%20%22$aggregate%22:%20[%22AVG:length%22]}
 // api-key
+
+// function fetchData() {
+//   fetch(
+//     "https://foobar-edfd.restdb.io/rest/foobar-queue?h={%22$groupby%22:[%22$HOUR:date%22],%20%22$aggregate%22:%20[%22AVG:length%22]}"
+//   )
+//     .then((res) => res.json)
+//     .then((data) => console.log(data));
+// }
+
+//console.log("QueueByHour Running");
 
 // Hvornår skal den fetche data? Evt når siden loader:
 
--- src/components/TapLevel.js
+++ src/components/TapLevel.js
@@ -1,15 +1,26 @@
 import React from "react";
 
 export default function TapLevel({ taps }) {
-  //Link to CSS-tricks article about progress-element: https://css-tricks.com/html5-progress-element/
+  //Link to CSS-tricks article about html5 progress: https://css-tricks.com/html5-progress-element/
   //console.log(taps);
+  let totalTapLeveL = 0;
+
+  taps.forEach((tap) => {
+    //console.log(tap);
+    totalTapLeveL += tap.level;
+  });
+  //console.log("TOTAL", totalTapLeveL);
+
   return (
     <>
       {taps.map((tap, index) => {
+        //console.log((tap.level / 100).toString(10).replace(".", ","));
         return (
           <React.Fragment key={index}>
             <label style={{ display: "block" }} htmlFor="tap-level">
-              Liter: {tap.level / 100} / {tap.capacity / 100}
+              {/* Liter: {tap.level / 100} / {tap.capacity / 100} */}
+              Liter: {(tap.level / 100).toString(10).replace(".", ",")} /{" "}
+              {tap.capacity / 100}
             </label>
             {/* <label style={{ display: "block" }} for="tap-level">
               Øl tilbage: {tap.level / 50}
@@ -19,9 +30,13 @@ export default function TapLevel({ taps }) {
               max={tap.capacity}
               value={tap.level}
             ></progress>
+            {/* {tap.level < 500 ? <p>Tap #{tap.id} er næsten tom</p> : null}
+            {tap.inUse ? <p>Tapper....</p> : <p>Står stille</p>} */}
           </React.Fragment>
         );
       })}
+      {/* <p>Samlet mængde tilbage: {totalTapLeveL / 50}</p> */}
+      {/* {totalTapLeveL < 1500 ? <p>Snart TOM</p> : null} */}
     </>
   );
 }
