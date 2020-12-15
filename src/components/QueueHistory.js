ebe23fdeb5c18cccca3e334886c2760f538423f1
Jonas <jona71e2@stud.kea.dk>
2020-12-15 05:28:52 +0100

    QueueHistory with comments

--- src/components/QueueHistory.js
+++ src/components/QueueHistory.js
@@ -1,35 +1,36 @@
 import React, { useState, useEffect } from "react";
-import { Bar, Line } from "react-chartjs-2";
+import { Line } from "react-chartjs-2";
 
-export default function QueueHistory({ queue, serving, tick }) {
+export default function QueueHistory({ queue, tick }) {
+  //HOOKS
   const [queueLength, setQueueLength] = useState([]);
   const [labelTime, setLabelTime] = useState([]);
 
-  //console.log("queueLength", queueLength, "FRA BACKEND:", queue.length);
-
-  //setQueueLength([...queueLength, queue.length]);
-
+  //useEffect kaldes ved første render og afhænger herefter af tick (defineret i App.js)
+  //tick bruges for at sikre en regelmæssig/lineær opdatering af data.
   useEffect(() => {
-    //console.log("queueLength", queueLength, "FRA BACKEND:", queue.length);
-    console.log(tick % 5);
+    //Her bruges remainder operator til at opdatere data, hver femte gang, App.js fetcher data
     if (tick % 5 === 0) {
-      setQueueLength(
+      setQueueLength((queueLength) =>
         queueLength.length === 15
           ? queueLength.slice(1)
           : [...queueLength, queue.length]
       );
-      setLabelTime(
+      setLabelTime((labelTime) =>
         labelTime.length === 15
           ? labelTime.slice(1)
           : [...labelTime, new Date(Date.now()).toString().split(" ")[4]]
       );
-      // function post ( => queue.length )
+      //Poster nuværende kølængde og tidspunkt til restdb (Bruges i QueueByHour komponent)
       function post(queue) {
-        console.log("POST", queue.length);
-
+        //Data object, der postes
         const data = {
           length: queue.length,
-          date: new Date(Date.now()).toString().split(" ")[4].substring(0, 5),
+          date: new Date(Date.now())
+            .toString() // ""
+            .split(" ") // ["",""]
+            [4] // [12:43:21]
+            .substring(0, 5), // 12:43
         };
         console.log("POST data object", data);
 
@@ -52,15 +53,12 @@ export default function QueueHistory({ queue, serving, tick }) {
           .then((data) => console.log(data));
       }
       // kald functionen post - som sender data til restdb
+      //udkommenteret, da der er data i databasen til at vise funktionaliteten
       //post(queue);
     }
-    //console.log("Inside useEffect", queueLength, labelTime);
   }, [tick]);
 
-  // useEffect(() => {
-  //   setQueueLength([...queueLength, queue.length]);
-  // });
-
+  //data og options objects bruges til at bygge chart vha. chartjs
   const data = {
     labels: labelTime,
     datasets: [
@@ -95,15 +93,5 @@ export default function QueueHistory({ queue, serving, tick }) {
     },
   };
 
-  return (
-    <>
-      {/* {queue.map((item) => {
-        const time = new Date(item.startTime).toString().split(" ");
-        console.log("KØTID - START", time);
-        return <p>Starttid: {time[4]}</p>;
-      })}
-      <p>HELLO</p> */}
-      <Line data={data} options={options} />
-    </>
-  );
+  return <Line data={data} options={options} />;
 }
