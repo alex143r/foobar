8bfee9d22d3862a1dbbf674cf1f7840a814e83fc
Jonas <jona71e2@stud.kea.dk>
2020-12-15 05:29:33 +0100

    markup change to Storage.js

--- src/components/Storage.js
+++ src/components/Storage.js
@@ -5,12 +5,13 @@ import ListContainer from "./ListContainer";
 
 export default function Storage({ taps, storage }) {
   return (
-    <>
+    <section className="beholdning">
+      <h1>BEHOLDNING</h1>
       {storage !== undefined ? (
         <ListContainer storage={storage} taps={taps} />
       ) : (
         "Loading"
       )}
-    </>
+    </section>
   );
 }
