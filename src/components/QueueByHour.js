3e10079b3214a06b03c831dfbc4d04ae80c3836a
Jonas <jona71e2@stud.kea.dk>
2020-12-17 06:14:34 +0100

    comments added

--- src/components/QueueByHour.js
+++ src/components/QueueByHour.js
@@ -2,14 +2,17 @@ import React, { useEffect, useState } from "react";
 import { Bar } from "react-chartjs-2";
 
 export default function QueueByHour() {
+  //HOOKS - array til brug i chartjs
   const [queueAverage, setQueueAverage] = useState([]);
   const [hour, setHour] = useState([]);
 
+  //Endpoint og api-key til restdb-database
   const url =
     "https://foobar-edfd.restdb.io/rest/foobar-queue?h={%22$groupby%22:[%22$HOUR:date%22],%20%22$aggregate%22:%20[%22AVG:length%22]}";
   const key = "5f9c2a96231ba42851b49f89";
 
   useEffect(() => {
+    //Function der fetcher data
     function fetchData() {
       fetch(url, {
         method: "get",
@@ -21,6 +24,7 @@ export default function QueueByHour() {
       })
         .then((e) => e.json())
         .then((data) => {
+          //Data sendes til functionen handleData
           handleData(data);
         });
     }
@@ -30,6 +34,7 @@ export default function QueueByHour() {
   function handleData(data) {
     for (const property in data) {
       console.log(`${property}: ${data[property]["AVG length"]}`);
+      //States sættes med dataen
       setQueueAverage((queueAverage) =>
         queueAverage.concat(data[property]["AVG length"].toFixed(2))
       );
@@ -37,6 +42,7 @@ export default function QueueByHour() {
     }
   }
 
+  //Objecter som passes til <Bar> komponent (chartjs)
   const data = {
     labels: hour,
     datasets: [
